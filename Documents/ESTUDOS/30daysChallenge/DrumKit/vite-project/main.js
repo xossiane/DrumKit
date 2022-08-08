239efad1c6afbc40f06bd3cd24c5887d32ecd4e0
xossiane <anejosi.lima@gmail.com>
2022-08-08 16:33:09 -0300

    Script alocado no main.js

--- Documents/ESTUDOS/30daysChallenge/DrumKit/vite-project/main.js
+++ Documents/ESTUDOS/30daysChallenge/DrumKit/vite-project/main.js
@@ -4,3 +4,25 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello :) </h1>`
 
 document.querySelector('#div').innerHTML = ` <h1>this is our DrumKit </h1>`
+window.addEventListener('keydown', function(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  
+ console.log(audio)
+ if (!audio) return;
+ audio.currentTime = 0; //não espera o áudio chegar até o final
+ 
+ audio.play();
+ key.classList.add('playing');
+ 
+ function removeTransition(e){
+   key.classList.remove('playing');
+ }
+ 
+ const keys = document.querySelectorAll('.key');
+ keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+ 
+ 
+ 
+ })
+   
