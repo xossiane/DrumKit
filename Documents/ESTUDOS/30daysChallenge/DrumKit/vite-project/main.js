69673fb0e2dd7924e9dd2d5ef09fc9f5b07b2adb
xossiane <anejosi.lima@gmail.com>
2022-08-08 14:47:16 -0300

    Acrescentando sons

--- Documents/ESTUDOS/30daysChallenge/DrumKit/vite-project/main.js
+++ Documents/ESTUDOS/30daysChallenge/DrumKit/vite-project/main.js
@@ -1,7 +1,6 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite! :) </h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
-document.querySelector('#div').innerHTML = ` <h1> HEY </h1>`
+  <h1>Hello :) </h1>`
+
+document.querySelector('#div').innerHTML = ` <h1>this is our DrumKit </h1>`
